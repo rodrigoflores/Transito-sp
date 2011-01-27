56d1945c73f19675f676c8a50cd3bd9b0e8229b0
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-01-27 19:31:49 -0200

    Changing the color logic

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  
+
   def status_color(number)
     number = number.to_i
-    number <= 60 ? :green : ( number <= 130? :yellow : :red )
+    number <= 25 ? :green : ( number <= 50? :yellow : :red )
   end
 end
